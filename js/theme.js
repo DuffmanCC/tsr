eed7a374f6e2a97b4760b1f666420346659ebf22
DuffmanCC <carlosor@gmail.com>
2015-08-22 14:05:51 +0200

    smooth scroll

--- js/theme.js
+++ js/theme.js
@@ -24,6 +24,22 @@ jQuery(document).ready(function($){
 		$('#site-navigation').addClass('fixed');
 	}
 
+	/* Scroll to specific section on front page */
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: (target.offset().top - 0)
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
 });
 
 
